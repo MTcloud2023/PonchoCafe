fdaf9b62bb6dfb0d017db6a0748d6e230dafb28a
MTcloud2023 <admon.montajes.t@gmail.com>
2025-07-11 18:36:35 +0200

    datios a

--- principal/settings.py
+++ principal/settings.py
@@ -121,6 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'inicio' / 'static',  # O ajusta al nombre de tu app
+]
 # This production code might break development mode, so we check whether we're in DEBUG mode
 if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
