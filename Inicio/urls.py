e4eb1d241f85fad2b5ebca294267a3b1a97a40e5
MTcloud2023 <admon.montajes.t@gmail.com>
2025-07-11 02:38:07 +0200

    primero

---
+++ Inicio/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path 
+from . import views
+urlpatterns = [
+    path('', views.hola),
+    path('about/',views.acercade),
+    path('polityka-prywatnosci/',views.privacidad),
+    path('menu/',views.menu),
+    path('kontakt/',views.contacto),
+    path('rezerwacje/',views.reserva, name='reserva'),
+    path('o-klubie/',views.acercade),
+
+]
---
+++ Inicio/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render 
+from django.http import HttpResponse
+from datetime import date
+
+def hola(request):
+        return render(request, 'inicio.html')
+    
+def acercade(request):
+    return render(request, 'about.html')
+
+def menu(request):
+    return render(request, 'menu.html')
+
+def privacidad (request):
+    return render(request, 'pdp.html')
+
+def reserva (request):
+    hoy = date.today().isoformat()
+    return render(request, 'reserva.html',{'hoy': hoy})
+def contacto (request):
+ return render(request, 'contacto.html')  
+    
