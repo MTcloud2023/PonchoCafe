816b74ec2fc4ba799d40073752bb50569b403dad
MTcloud2023 <admon.montajes.t@gmail.com>
2025-07-14 11:05:56 +0200

    urls referenciadas

--- Inicio/urls.py
+++ Inicio/urls.py
@@ -2,14 +2,14 @@ from django.urls import path, re_path
 from django.shortcuts import redirect
 from . import views
 urlpatterns = [
-    path('', views.hola),
-    path('about/',views.acercade),
-    path('polityka-prywatnosci/',views.privacidad),
-    path('menu/',views.menu),
-    path('kontakt/',views.contacto),
-    path('rezerwacje/',views.reserva, name='reserva'),
-    path('o-klubie/',views.acercade),
-    path('coming-soon/', views.comingsoon, name='coming_soon'),
+    #path('', views.hola),
+    #path('about/',views.acercade),
+    #path('polityka-prywatnosci/',views.privacidad),
+    #path('menu/',views.menu),
+    #path('kontakt/',views.contacto),
+    #path('rezerwacje/',views.reserva, name='reserva'),
+    #path('o-klubie/',views.acercade),
+    #path('coming-soon/', views.comingsoon, name='coming_soon'),
     re_path(r'^.*$', lambda request: redirect('coming_soon')),
 
 
